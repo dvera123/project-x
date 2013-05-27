e5c832f26751a9372bdb6429b6ca85fb03b8064a
Diego Vera <Vera@172.16.30.84>
2013-05-27 08:31:07 -0600

    [PX00]:settings

---
+++ src/main/java/com/projectx/domain/Step.java
@@ -0,0 +1,133 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.projectx.domain;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Vera
+ */
+@Entity
+@Table(name = "step")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Step.findAll", query = "SELECT s FROM Step s"),
+    @NamedQuery(name = "Step.findById", query = "SELECT s FROM Step s WHERE s.id = :id"),
+    @NamedQuery(name = "Step.findByNum", query = "SELECT s FROM Step s WHERE s.num = :num"),
+    @NamedQuery(name = "Step.findByDescription", query = "SELECT s FROM Step s WHERE s.description = :description"),
+    @NamedQuery(name = "Step.findByAction", query = "SELECT s FROM Step s WHERE s.action = :action")})
+public class Step implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @Column(name = "num")
+    private int num;
+    @Basic(optional = false)
+    @Column(name = "description")
+    private String description;
+    @Basic(optional = false)
+    @Column(name = "action")
+    private String action;
+    @JoinColumn(name = "id", referencedColumnName = "id", insertable = false, updatable = false)
+    @OneToOne(optional = false)
+    private StepStatus stepStatus;
+
+    public Step() {
+    }
+
+    public Step(Integer id) {
+        this.id = id;
+    }
+
+    public Step(Integer id, int num, String description, String action) {
+        this.id = id;
+        this.num = num;
+        this.description = description;
+        this.action = action;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public StepStatus getStepStatus() {
+        return stepStatus;
+    }
+
+    public void setStepStatus(StepStatus stepStatus) {
+        this.stepStatus = stepStatus;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Step)) {
+            return false;
+        }
+        Step other = (Step) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.projectx.domain.Step[ id=" + id + " ]";
+    }
+    
+}
---
+++ src/main/java/com/projectx/util/HibernateUtil.java
@@ -0,0 +1,32 @@
+package com.projectx.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
+
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+        	Configuration configuration = new Configuration();
+            configuration.configure("/com/spring/project/util/hibernate.cfg.xml");
+            serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
+
+
