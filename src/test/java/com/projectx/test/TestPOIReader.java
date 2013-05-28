baa5c5a518abe3b9f06a72706c282171a993c4e2
Diego Vera <Vera@172.16.30.84>
2013-05-28 08:40:19 -0600

    [PX06]:poi reader library added.

---
+++ src/test/java/com/projectx/test/TestPOIReader.java
@@ -0,0 +1,14 @@
+package com.projectx.test;
+
+import org.junit.Test;
+
+import com.projectx.poi.util.POIReader;
+
+public class TestPOIReader {
+	
+	@Test
+	public void poiReaderTest(){
+		
+	}
+
+}
--- src/test/java/com/projectx/test/TestStep.java
+++ src/test/java/com/projectx/test/TestStep.java
@@ -1,18 +1,24 @@
 package com.projectx.test;
 
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.junit.Test;
+
+import com.projectx.util.HibernateUtil;
 
 
 public class TestStep {
 	
 	@Test
 	public void getStepById(){
-		/*Session session = HibernateUtil.getSessionFactory().openSession();
+		Session session = HibernateUtil.getSessionFactory().openSession();
 		Query qr = session.getNamedQuery("TestCase.findAll");
         List l = qr.list();
         for(Object item: l){
         	System.out.println(item.toString());
-        }*/
+        }
         
 	}
 
